bb7066e2f772b330feea4092721f5c5be293f6f9
rabiulim <rabiulim2022@gmail.com>
2022-04-24 11:51:03 +0600

    car service client without update

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCFVwXb72kYLw7UKT4jXVPKgil-eaKiJrA",
+  authDomain: "genius-car-services-f036c.firebaseapp.com",
+  projectId: "genius-car-services-f036c",
+  storageBucket: "genius-car-services-f036c.appspot.com",
+  messagingSenderId: "315044439089",
+  appId: "1:315044439089:web:7e7ec8d6fb2bb6f960a110"
 };
 
 // Initialize Firebase
